dd500f6db4a7523fd771859356bb522a4fa6354c
Fernando Falcon <ffalconz@gmail.com>
2025-02-10 09:26:41 -0600

    Remplazo del codigo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div className="box1">
+        <p>Hola mundo</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbarstyles.css';
+
+
+const Navbar = () => {
+  return (
+    <div className='chivas'>
+        <nav className='tigres'>
+        <ul className='america'>
+            <li className='necaxa'>Hola</li>
+            <li>Adios</li>
+        </ul>
+    </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
